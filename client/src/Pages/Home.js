2522e666f4f10267d46ff43f18f8b2617a9da978
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-12-24 15:11:01 -0800

    Added basic layout to the homepage

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import Carrier from '../Imgs/Carrier.jpg';
 import '../Styling//Home.css';
 
 class Home extends React.Component {
@@ -9,7 +10,30 @@ class Home extends React.Component {
 
     render() {
         return (
-            <div>Hello</div>
+            <div>
+                <div className="homepage-container">
+                    {/* The panel on the homepage, contains background image, signup button */}
+                    <section className="panel" >
+                        <div className="panel-image">
+                            <div className="panel-description">
+                                {/* The text display on the home page */}
+                                <div className="panel-description-background">
+                                    <h2>Returning Packages Simplified.</h2>
+                                    <button>Sign up</button>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+
+                <div className="homepage-container">
+                    <section className="panel" >
+                        <div className="panel-image">
+                            
+                        </div>
+                    </section>
+                </div>
+            </div>
         )
     }
 }
