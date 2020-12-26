688f6b1570e434dd3574b43811324ed5b90d72f9
Amitesh Sharma <amitesh.sharma.2001@gmail.com>
2020-12-25 23:45:42 -0800

    front page design almost complete, will work on changing navbar when scrolling and then work on desinging backend

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -12,25 +12,41 @@ class Home extends React.Component {
     render() {
         return (
             <div>
+                {/* The navbar for the homepage and all pages */}
                 <NavBar />
                 <div className="homepage-container">
                     {/* The panel on the homepage, contains background image, signup button */}
                     <section className="panel" >
                         <div className="panel-image">
-                            <img className="panel-vector-img" src={Carrier} width={"100%"} height={"100%"} />    
+                            <img className="panel-vector-img" src={Carrier} width={"100%"} height={"930"} />    
                         </div>
                     </section>
+
+                    {/* Floating cloud for design purposes */}
+                    <section className="cloud-section">
+                        <div className="cloud"></div>
+                    </section>
+
+                    {/* The homepage banner with signup and login button */}
+                    <section className="banner">
+                        <div className="cloud">
+                            <h2>Returning Simplified</h2>
+                        </div>
+                    </section>
+
+                    <section className="description">
+                        <div className="summary">
+                            <p>Return packages without the hassle. In 4 simple steps, return packages without grabbing your keys or driving to the nearest post office.</p>
+                        </div>
+                    </section>
+                </div>
+
+                <div>
+                    <h1>HELLO WORLD</h1>
                 </div>
             </div>
         )
     }
 }
-
-{/* <div className="panel-description">
-    <div className="panel-description-background">
-        <h2>Returning Packages Simplified.</h2>
-        <button>Sign up</button>
-    </div>
-</div> */}
 
 export default Home;
